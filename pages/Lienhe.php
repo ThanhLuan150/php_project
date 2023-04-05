139172b905a0f3dea0c7655d83fe6c9569b0efdd
a du <du.a24@student.passerellesnumeriques.org>
2023-04-05 15:08:09 +0700

    done

--- pages/Lienhe.php
+++ pages/Lienhe.php
@@ -93,8 +93,9 @@
                             <input style="width:900px" type="text"  id="sdt" name="sdt"><br><br>
                             <label for="fname">Nội dung</label><br>
                             <textarea style="width:900px" name="" id="" cols="30" rows="10"></textarea> <br><br>
-                             <input type="button" value="Gửi nội dung">
+                            <input type="button" class="button-fromt" value="Gửi nội dung">
                         </form>
+                        <h2 class="h2chiduong">CHỈ ĐƯỜNG ĐI ĐẾN ÁO CỔ PHỤC TTDVL</h2>
                         <div class="wpb_map_wraper"><iframe style="width:900px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.1123473457783!2d108.2385929489041!3d16.059658788832326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142177f0057f675%3A0xdec5dbe31838c835!2zMzY1UitWOUcsIDEwMUIgTMOqIEjhu691IFRyw6FjLCBQaMaw4bubYyBN4bu5LCBTxqFuIFRyw6AsIMSQw6AgTuG6tW5nIDU1MDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1680341349914!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
                     </div>
             </div>
