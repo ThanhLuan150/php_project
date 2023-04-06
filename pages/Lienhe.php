c9aecb08b92ea68b7531216030f114381ed37f1f
a du <du.a24@student.passerellesnumeriques.org>
2023-04-06 10:25:03 +0700

    dangkydangnhaplienhe

---
+++ pages/Lienhe.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/lienhe.css">
+    <link rel="stylesheet" href="/bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <script src="/bootstrap-5.2.2-dist/js/jquery.min.js"></script>
+    <script src="/bootstrap-5.2.2-dist/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+        <div class="header">
+            <div class="image">
+                <img class="img" src="../img/logo (1).jpg" alt="">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="banggiaphucvu.php">BẢNG GIÁ PHỤC VỤ</a></li>
+                    <li><a href="aodoikham.php">ÁO ĐỐI KHÂM</a></li>
+                    <li><a href="aogiaolinh.php">ÁO GIAO LĨNH</a></li>
+                    <li><a href="aonhatbinh.php">ÁO NHẬT BÌNH</a></li>
+                    <li><a href="aotac.php">ÁO TẮC</a></li>
+                    <li><a href="aonguthan.php">ÁO NGŨ THÂN</a></li>
+                    <li><a href="blog.php">BLOG</a></li>
+                    <li><a href="lienhe.php">LIÊN HỆ</a></li>
+                </ul>
+            </div>
+            <div class="function">
+                <div class="functionsearch">
+                    <div class="search">
+                        <i  class="fa-sharp fa-solid fa-magnifying-glass" data-toggle="modal" data-target="#myModal"></i>
+                        <div class="modal fade" id="myModal" role="dialog">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Tìm kiếm</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <input class="input" type="search" name="search" value="" placeholder="Nhập khóa">
+                                        <button class="button" type="submit" name="submit">Tìm kiếm</button>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="button" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+                <button class="button"><a  href="themgiohang.php">Giỏ hàng <i class="fa-sharp fa-solid fa-cart-shopping"></i> </a></button>
+                <button class="button"><a  href="signin.php">Đăng nhập</a></button>
+                <button class="button"><a  href="signin.php">Đăng ký</a></button>
+            </div>
+        </div>
+        <div class="container-boy">
+            <h1 class="element-title">LIÊN HỆ</h1>
+            <div class="title2">
+                <a class="title-name" href="#">Home /</a> 
+                <span class="current">LIÊN HỆ</span>
+            </div>																	
+        </div>
+        <div id="content" class="main">
+
+            <div id="post">
+            <div class="content-name">
+                        <h2>ÁO CỔ PHỤC TTDVL</h2>
+                                    <img class="" src="../img/logo (1).jpg" alt="" width="100px" height="100px" data-wp-editing="1">
+                                    <p class="content22"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 1: 101b, Lê Hữu Trác , Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                                    <p class="content22"><i class="fa-solid fa-phone"></i>   Phone: 0702307677 ( Mr Trung)</p>
+                                    <p class="content22"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 2: 99 Tô Hiến Thành, Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                                    <p class="content22"><i class="fa-solid fa-phone"></i>   Phone: 0787628654 (Mr Luân)</p>
+                                    <p class="content22"><i class="fa-solid fa-envelope"></i>   Email: aocophucttdvl@gmail.com</p>
+                                    <p class="content22"><i class="fa-regular fa-calendar-pen"></i>   Giờ mở cửa: 8h30 đến 21h30 Từ thứ 2 đến Chủ nhật</p>
+                    </div>
+                    <div class="content-fromt">
+                        <form>
+                            <label for="fname">Tên của bạn </label><br>
+                            <input style="width:900px" type="text"  id="fname" name="fname"><br><br>
+                            <label for="fname">Email</label><br>
+                            <input style="width:900px" type="text"  id="email" name="email"><br><br>
+                            <label for="fname">Số điệnthoại</label><br>
+                            <input style="width:900px" type="text"  id="sdt" name="sdt"><br><br>
+                            <label for="fname">Nội dung</label><br>
+                            <textarea style="width:900px" name="" id="" cols="30" rows="10"></textarea> <br><br>
+
+                             <input type="button" class="button-fromt" value="Gửi nội dung">
+                        </form>
+                        <h2 class="h2chiduong">CHỈ ĐƯỜNG ĐI ĐẾN ÁO CỔ PHỤC TTDVL</h2>
+                        <div class="wpb_map_wraper"><iframe style="width:900px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.1123473457783!2d108.2385929489041!3d16.059658788832326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142177f0057f675%3A0xdec5dbe31838c835!2zMzY1UitWOUcsIDEwMUIgTMOqIEjhu691IFRyw6FjLCBQaMaw4bubYyBN4bu5LCBTxqFuIFRyw6AsIMSQw6AgTuG6tW5nIDU1MDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1680341349914!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                    </div>
+            </div>
+
+            <div id="sidebar">Nội dung của #sidebar</div>
+<div></div>
+            </div>
+        <!-- <div  class="main">
+            
+                    <div class="content-name">
+                        <h2><strong>ÁO CỔ PHỤC TTDVL</strong></h2>
+                                    <img class="" src="../img/logo (1).jpg" alt="" width="100px" height="100px" data-wp-editing="1">
+                                    <p class="content11"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 1: 101b, Lê Hữu Trác , Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                                    <p class="content11"><i class="fa-solid fa-phone"></i>   Phone: 0702307677 ( Mr Trung)</p>
+                                    <p class="content11"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 2: 99 Tô Hiến Thành, Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                                    <p class="content11"><i class="fa-solid fa-phone"></i>   Phone: 0787628654 (Mr Luân)</p>
+                                    <p class="content11"><i class="fa-solid fa-envelope"></i>   Email: aocophucttdvl@gmail.com</p>
+                                    <p class="content11"><i class="fa-regular fa-calendar-pen"></i>   Giờ mở cửa: 8h30 đến 21h30 Từ thứ 2 đến Chủ nhật</p>
+                    </div>
+                    <div class="content-fromt">
+                        <form>
+                            <label for="fname">Tên của bạn </label><br>
+                            <input style="width:900px" type="text"  id="fname" name="fname"><br><br>
+                            <label for="fname">Email</label><br>
+                            <input style="width:900px" type="text"  id="email" name="email"><br><br>
+                            <label for="fname">Số điệnthoại</label><br>
+                            <input style="width:900px" type="text"  id="sdt" name="sdt"><br><br>
+                            <label for="fname">Nội dung</label><br>
+                            <textarea style="width:900px" name="" id="" cols="30" rows="10"></textarea> <br><br>
+                             <input type="button" value="Gửi nội dung">
+                        </form>
+                        <div class="wpb_map_wraper"><iframe style="width:900px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.1123473457783!2d108.2385929489041!3d16.059658788832326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142177f0057f675%3A0xdec5dbe31838c835!2zMzY1UitWOUcsIDEwMUIgTMOqIEjhu691IFRyw6FjLCBQaMaw4bubYyBN4bu5LCBTxqFuIFRyw6AsIMSQw6AgTuG6tW5nIDU1MDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1680341349914!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                    </div>
+                    
+                </div> -->
+    
+    <div class="footer">
+        <div class="content">
+            <div class="content1">
+<p class="content11">ÁO CỔ PHỤC TTDVL</p>
+                <p class="content11"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 1: 101b, Lê Hữu Trác , Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                <p class="content11"><i class="fa-solid fa-phone"></i>   Phone: 0702307677 ( Mr Trung)</p>
+                <p class="content11"><i class="fa-solid fa-location-dot"></i>   Chi Nhánh 2: 99 Tô Hiến Thành, Phước Mỹ, Sơn Trà, Đà Nẵng</p>
+                <p class="content11"><i class="fa-solid fa-phone"></i>   Phone: 0787628654 (Mr Luân)</p>
+                <p class="content11"><i class="fa-solid fa-envelope"></i>   Email: aocophucttdvl@gmail.com</p>
+                <p class="content11"><i class="fa-regular fa-calendar-pen"></i>   Giờ mở cửa: 8h30 đến 21h30 Từ thứ 2 đến Chủ nhật</p>
+            </div>
+            <div class="content1">
+                <p class="content11">DỊCH VỤ CHỤP ẢNH</p>
+                <p class="content11">Bảng giá thuê áo cổ phục</p>
+                <p class="content11">Thuê  áo đối khâm</p>
+                <p class="content11">Thuê áo giao lĩnh</p>
+                <p class="content11">Thuê áo nhật bình</p>
+                <p class="content11">Thuê áo tắc</p>
+                <p class="content11">Thuê áo ngũ thân</p>
+            </div>
+            <div class="content1">
+                <p class="content11">GIỚI THIỆU</p>
+                <p class="content11">Giói thiệu cho thuê áo cổ phục</p>
+                <p class="content11">Áo đối khâm</p>
+                <p class="content11">Áo giao lĩnh</p>
+                <p class="content11">Áo nhật bình</p>
+                <p class="content11">Áo tấc</p>
+                <p class="content11">Áo ngũ thân</p>
+                <p class="Content">Liên hệ</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+       
+       
+
+</body>
+</html>
