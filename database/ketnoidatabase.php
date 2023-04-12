f5976ae56f16c444aef7690bbd53004cd5f24530
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-04-12 05:07:59 -0800

    database

---
+++ database/ketnoidatabase.php
@@ -0,0 +1,12 @@
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "rent_clothes";
+
+ // Create connection
+ $mysqli = new mysqli($servername, $username, $password, $dbname);
+ if ($mysqli === false) {
+     die("ERROR: Could not connect. " . $mysqli->connect_error);
+ }
+
